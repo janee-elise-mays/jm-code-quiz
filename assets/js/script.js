f482cd22812aac3a3c8f051428cacb3e25a5769d
Janeé Mays <janee.elise@gmail.com>
2021-02-19 18:31:25 -0500

    added timer limitations and score count

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 var startBtn = document.getElementById('startBtn') 
 var timeleft = 75;
 var timeleftDisplay = document.getElementById('timer')
+var correctAnswers = ['Start','D', 'A', 'A', 'D', 'B'];
+var currentQuestion = 0;
+var score = 1;
 
 function countDown(){
     setInterval(function(){
         if(timeleft <= 0 ) {
             clearInterval(timeleft = 0)
+            document.getElementById('question-box-6').style.visibility = 'visible';
+        }
+        if(currentQuestion == 6 ) {
+            clearInterval(timeleft = 0);
         }
         timeleftDisplay.innerHTML = 'time: '+timeleft; 
         timeleft--;
@@ -15,9 +22,6 @@ function countDown(){
 startBtn.addEventListener('click',function(){countDown()});
 
 
-var correctAnswers = ['Start','D', 'A', 'A', 'D', 'B'];
-var currentQuestion = 0;
-
 document.getElementById('question-box-' + currentQuestion).style.visibility = 'visible';
 
 function checkAnswer(event) {
@@ -25,17 +29,18 @@ function checkAnswer(event) {
 
     if (correctAnswers[currentQuestion] != answer && currentQuestion !=0)  {
         timeleft=timeleft-10;
-
     }
 
     // check answer against correctAnswer
     // if it's correct, you'll add to their score and move onto the next question
-    // if they aren't correct, you'll subtract 10 from their time (move onto next)
+    if (correctAnswers[currentQuestion] == answer && currentQuestion !=0)  {
+        score++;
+    }
 
     document.getElementById('question-box-' + currentQuestion).style.visibility = 'hidden';
     currentQuestion += 1;
     document.getElementById('question-box-' + currentQuestion).style.visibility = 'visible';
 }
-
-
-//  after clicking the start button a timer will start and the first question will appear
+//  function showScore(){
+//      document.getElementById('generate').innerHTML = 
+//  }
